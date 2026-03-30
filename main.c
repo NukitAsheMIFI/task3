bd7ea964fc193a82ac59e3f9e8cfd781cd4b8986
Ivan <ivan@NOTEBOOKDELLG3.localdomain>
2026-03-30 13:07:40 +0300

    сырая версия

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 
 #define BUF_SIZE 64 
 
-void plane(char **arr, size_t len, int dir);
+void plane(char **arr, size_t arr_size, int dir);
+void lex(char **arr, size_t arr_size, int dir);
 
 int main(int argc, char **argv){
 	if (argc < 4){
@@ -23,16 +24,19 @@ int main(int argc, char **argv){
 	FILE* out_file = fopen(out_name, "w");
 	if (out_file == NULL){
 		perror("fopen error");
+		fclose(in_file);
 		return EXIT_FAILURE;
 	}
 
 	fseek(in_file, 0, SEEK_END); //указатель в конец файла
 	size_t len = ftell(in_file); //длина текста в файле
+	rewind(in_file); //указатель в начало
 	char *text = malloc(len + 1);
-
 	size_t bytes_read = fread(text, 1, len, in_file); //считываем текст из файла
-	if (bytes_read < len) {
+	if (bytes_read < len) {;
 		perror("fread error");
+		fclose(in_file);
+		fclose(out_file);
 		free(text);
 		return EXIT_FAILURE;
 	}
@@ -40,9 +44,11 @@ int main(int argc, char **argv){
 	text[len] = '\0'; //делаем строку
 	
 	size_t arr_size = 5;
-	char **arr = malloc(arr_size * 1);
+	char **arr = malloc(arr_size * sizeof(char*));
 	if (arr == NULL){
 		perror("allocation memory error");
+		fclose(in_file);
+		fclose(out_file);
 		free(text);
 		return EXIT_FAILURE;
 	}
@@ -53,10 +59,11 @@ int main(int argc, char **argv){
 
 		if (i + 1 > (int)arr_size){
 			arr_size *= 2;
-			char **temp_arr = realloc(arr, arr_size);
-			if (arr == NULL){
+			char **temp_arr = realloc(arr, arr_size *  sizeof(char*));
+			if (temp_arr == NULL){
 				perror("allocation memory error");
-				free(arr);
+				fclose(in_file);
+				fclose(out_file);
 				free(text);
 				return EXIT_FAILURE;
 			}	
@@ -68,39 +75,63 @@ int main(int argc, char **argv){
 	}
 
 	arr_size = i;
-	char **temp_arr = realloc(arr, arr_size);
-	if (arr == NULL){
+	char **temp_arr = realloc(arr, arr_size *  sizeof(char*));
+	if (temp_arr == NULL){
 		perror("allocation memory error");
-		free(arr);
+		fclose(in_file);
+		fclose(out_file);
 		free(text);
 		return EXIT_FAILURE;
 	}	
 	arr = temp_arr;
-	free(text);
 
-	if (strcmp(mod, "plane")) {
-		plane(arr, len, 1);
+	if (strcmp(mod, "plane") == 0) {
+		plane(arr, arr_size, 1);
 	} 
-	else if(strcmp(mod, "rplane")) {
-		plane(arr, len, -1);
+	else if(strcmp(mod, "rplane") == 0) {
+		plane(arr, arr_size, -1);
+	}
+	else if(strcmp(mod, "lex") == 0){
+		lex(arr, arr_size, 1);
+	}
+	else if(strcmp(mod, "rlex") == 0){
+		lex(arr, arr_size, -1);
 	}
 	else {
 		printf("неизвестная сортировка\n");
+		fclose(in_file);
+		fclose(out_file);
 		free(arr);
 		return EXIT_SUCCESS;
 	}
 
-	for (size_t i = 0; i < len; i++){
-		printf("%s ", arr[i]);
+	for (size_t i = 0; i < arr_size; i++){
+		fprintf(out_file, "%s ", arr[i]);
 	}
-	printf("\n");
+	free(text);
+	fclose(in_file);
+	fclose(out_file);
 	return EXIT_SUCCESS;
 	
 }
 
-void plane(char **arr, size_t len, int dir){ //сортировка пузырьком с параметром направления
-	for (size_t i = 0; i + 1 < len; i++) {
-		for (size_t j = 0; j + 1 < len - i; j++) {
+void lex(char **arr, size_t arr_size, int dir){
+	for (size_t i = 0; i + 1 < arr_size; i++) {
+		for (size_t j = 0; j + 1 < arr_size - i; j++) {
+			int res = strcmp(arr[j], arr[j+1]);
+			res *= dir;
+			if (res > 0){
+				char *temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;		
+			}
+		}
+	}
+}
+
+void plane(char **arr, size_t arr_size, int dir){ //сортировка пузырьком с параметром направления
+	for (size_t i = 0; i + 1 < arr_size; i++) {
+		for (size_t j = 0; j + 1 < arr_size - i; j++) {
 			size_t k = 0;
 			while (arr[j][k] != '\0' && arr[j][k] == arr[j + 1][k]){	
 				k++;
@@ -112,7 +143,7 @@ void plane(char **arr, size_t len, int dir){ //сортировка пузырь
 				swap = 1;
 			}
 			else if (dir == -1 && arr[j][k] < arr[j + 1][k]) {
-				
+				swap = 1;
 			}
 			if (swap) {
 				char *temp = arr[j];
